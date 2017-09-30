a48fc19cba8e4c5351d1dfaf80dd50b508bb04ed
maxlmo <max.moeller55@gmail.com>
2017-10-01 01:06:30 +0200

    Tslint project

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,24 @@
 /**
  * Module dependencies.
  */
-import * as express from "express";
-import * as compression from "compression";  // compresses requests
-import * as session from "express-session";
-import * as bodyParser from "body-parser";
-import * as logger from "morgan";
-import * as errorHandler from "errorhandler";
-import * as dotenv from "dotenv";
-import * as path from "path";
-
+import * as express from 'express';
+import * as compression from 'compression';  // compresses requests
+import * as session from 'express-session';
+import * as bodyParser from 'body-parser';
+import * as logger from 'morgan';
+import * as errorHandler from 'errorhandler';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
  */
-dotenv.config({ path: ".env.example" });
+dotenv.config({ path: '.env.example' });
 
 
 /**
  * Controllers (route handlers).
  */
-import * as homeController from "./controllers/home";
+import * as homeController from './controllers/home';
 
 /**
  * Create Express server.
@@ -30,11 +29,10 @@ const app = express();
 /**
  * Express configuration.
  */
-app.set("port", process.env.PORT || 3000);
-app.set("views", path.join(__dirname, "../views"));
-app.set("view engine", "pug");
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'pug');
 app.use(compression());
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
@@ -46,35 +44,19 @@ app.use((req, res, next) => {
   res.locals.user = req.user;
   next();
 });
-app.use((req, res, next) => {
-  // After successful login, redirect back to the intended page
-  if (!req.user &&
-      req.path !== "/login" &&
-      req.path !== "/signup" &&
-      !req.path.match(/^\/auth/) &&
-      !req.path.match(/\./)) {
-    req.session.returnTo = req.path;
-  } else if (req.user &&
-      req.path == "/account") {
-    req.session.returnTo = req.path;
-  }
-  next();
-});
-app.use(express.static(path.join(__dirname, "public"), { maxAge: 31557600000 }));
-
 /**
  * Primary app routes.
  */
-app.get("/", homeController.index);
+app.get('/', homeController.index);
 
 app.use(errorHandler());
 
 /**
  * Start Express server.
  */
-app.listen(app.get("port"), () => {
-  console.log(("  App is running at http://localhost:%d in %s mode"), app.get("port"), app.get("env"));
-  console.log("  Press CTRL-C to stop\n");
+app.listen(app.get('port'), () => {
+  console.log(('  App is running at http://localhost:%d in %s mode'), app.get('port'), app.get('env'));
+  console.log('  Press CTRL-C to stop\n');
 });
 
 module.exports = app;
